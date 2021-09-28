a3ee038cb597d4a18dafcee63b2f982f22c0d7a7
Ange121 <ttareas982@gmail.com>
2021-09-28 12:35:24 -0500

    prueva0.1

--- ProyectoAED-final/src/cibertec/JdConsulCurso.java
+++ ProyectoAED-final/src/cibertec/JdConsulCurso.java
@@ -3,8 +3,17 @@ package cibertec;
 import java.awt.EventQueue;
 
 import javax.swing.JDialog;
+import javax.swing.JScrollPane;
+import javax.swing.JTextPane;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class JdConsulCurso extends JDialog {
+	private JTextPane txtJtexAreaResultado;
+	private JLabel lblNewLabel;
+	private JTextField textField;
+	private JButton btnConsultar;
 
 	/**
 	 * Launch the application.
@@ -28,6 +37,27 @@ public class JdConsulCurso extends JDialog {
 	 */
 	public JdConsulCurso() {
 		setBounds(180, 100, 553, 373);
+		getContentPane().setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 93, 517, 231);
+		getContentPane().add(scrollPane);
+		
+		txtJtexAreaResultado = new JTextPane();
+		scrollPane.setViewportView(txtJtexAreaResultado);
+		
+		lblNewLabel = new JLabel("Codigo de Curso :");
+		lblNewLabel.setBounds(10, 33, 108, 14);
+		getContentPane().add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(128, 30, 132, 20);
+		getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		btnConsultar = new JButton("Consultar");
+		btnConsultar.setBounds(438, 29, 89, 23);
+		getContentPane().add(btnConsultar);
 
 	}
 
